80781639d01f1d2cecb094cfdee5cc31edbc27fb
shahzaib <shahzaibceo@gmail.com>
2021-03-30 04:47:46 +0500

    Lab 3 done

--- src/main/scala/lab3/task3.scala
+++ src/main/scala/lab3/task3.scala
@@ -6,7 +6,7 @@ import chisel3.util._
 
 class LM_IO_Interface_decoder_with_valid extends Bundle {
     val in = Input(UInt(2.W))
-    val out = Valid(Output(UInt(4.W)))
+    val out = Valid(UInt(4.W))
 }
 
 class decoder_with_valid extends Module {
