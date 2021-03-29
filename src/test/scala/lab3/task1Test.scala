53db4ac8298fe7da3ba015e2e5209683d71a5172
shahzaib <shahzaibceo@gmail.com>
2021-03-30 04:47:46 +0500

    Lab 3 done

--- src/test/scala/lab3/task1Test.scala
+++ src/test/scala/lab3/task1Test.scala
@@ -9,11 +9,12 @@ import chiseltest.internal.VerilatorBackendAnnotation
 class Task1Test extends FreeSpec with ChiselScalatestTester {
     "Task 1 Test" in {
         test(new BranchControl).withAnnotations(Seq(VerilatorBackendAnnotation)) {c => 
-            c.io.funct3.poke("b111".U)
+            c.io.funct3.poke(5.U)
             c.io.branch.poke(true.B)
             c.io.arg_x.poke(3.U)
             c.io.arg_y.poke(2.U)
             c.clock.step(10)
+            c.io.br_taken.expect(1.B)
         }
     }
 }
